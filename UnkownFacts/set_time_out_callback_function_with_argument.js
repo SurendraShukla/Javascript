7597254a84c9ad81724debae968e0b12fc832825
Shukla, Surendra <surendra.shukla@cdk.com>
2017-03-29 02:08:20 +0530

    Things to know

---
+++ UnkownFacts/set_time_out_callback_function_with_argument.js
@@ -0,0 +1,7 @@
+function sayHi(phrase, who) {
+    console.log( phrase + ', ' + who );
+}
+
+setTimeout(sayHi, 1000, "Surendra", "Shukla"); // Hello, Surendra
+
+// http://javascript.info/settimeout-setinterval
