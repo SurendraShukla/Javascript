f32a25fb916cc2aa392b24719299f7c65d594d2a
Surendra Shukla <surenshukla8@gmail.com>
2018-08-11 18:05:30 -0400

    stringify changes

--- stringify.js
+++ stringify.js
@@ -1,3 +1,40 @@
 /**
- * Created by surendrashukla on 28/01/18.
- */
+ *  For this exercise you should implement a JSON "stringify" function
+ *  That will turn this simple object into a valid JSON string.
+ *  The result is tested against the output of the built-in JSON.stringify
+ **/
+
+var obj = {
+    foo: 'bar',
+    id: 1
+};
+
+function stringify(obj) {
+    // implement this
+
+    //The object has 2 prop
+    //"{"foo": "bar", "id":1}"
+    var result = '';
+
+    var size = Object.keys(obj).length;
+    var count = 0;
+    for(var i in obj) {
+        // obj[i] // obj["foo"]
+        result += '\"' + i + '\"';
+        result += ':';
+        if(typeof obj[i] === 'number') {
+            result += obj[i];
+        } else {
+            result += '\"' + obj[i] + '\"';
+        }
+        count++;
+        if(count < size) {
+            result += ','
+        }
+
+    }
+    return '{' + result + '}';
+}
+
+// should log true
+console.log(JSON.stringify(obj) === stringify(obj));
