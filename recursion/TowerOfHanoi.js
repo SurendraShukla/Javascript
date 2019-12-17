82e089f219a792bbdc77f2f76d51ed017713aad3
Shukla, Surendra <Surendra.Shukla@cdk.com>
2019-12-16 22:02:20 -0800

    tower of hanoi change

--- recursion/TowerOfHanoi.js
+++ recursion/TowerOfHanoi.js
@@ -1,51 +1,14 @@
-var plates = 6;
+var tower1 = [4, 3, 2, 1]; var tower2 = []; var tower3 = [];
 
-var tower1 = getRangeFilledInDescendingOrder(1, plates),
-	tower2 = [],
-	tower3 = [];
-
-console.log("Before Movement");
-console.log(tower1);
-console.log(tower2);
-console.log(tower3);
-toh(tower1, tower3, plates);
-console.log("After Movement");
-console.log(tower1);
-console.log(tower2);
-console.log(tower3);
-
-
-function toh(source, destination, noOfPlates) {
-
-	if(1 == noOfPlates) {
-		movePlate(source, destination);
+function move(n, source, target, auxiliary) {
+	if (n <= 0) {
 		return;
 	}
-	var midTower = getMidTower(source, destination);
-	toh(source, midTower, noOfPlates-1);
-	movePlate(source, destination);
-	toh(midTower, destination, noOfPlates-1);
-}
 
-
-function getMidTower(source, destination) {
-	var towerList = [tower1, tower2, tower3];
-	for (var i = 0; i < towerList.length; i++) {
-		var tower = towerList[i];
-		if( (tower!= source) && (tower != destination) ) {
-			return tower;
-		}
-	};
+	move(n-1, source, auxiliary, target);
+	target.push(source.pop());
+	console.log(tower1, tower2, tower3);
+	move(n-1, auxiliary, target, source);
 }
-
-function movePlate(source, destination) {
-	destination.push(source.pop());
-}
-
-function getRangeFilledInDescendingOrder(start, end) {
-	var rangeArray = [];
-	while(start <= end) {
-		rangeArray[rangeArray.length] = end--;
-	}
-	return rangeArray;
-}
+console.log(tower1, tower2, tower3);
+move(tower1.length, tower1, tower3, tower2);
